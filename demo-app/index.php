0a26464187f4b545ecc470d9252f0ea2b291dcd0
Skylar Kelty <skylarkelty@gmail.com>
2013-10-26 15:42:28 +0100

    New Demo App

--- demo-app/index.php
+++ demo-app/index.php
@@ -2,3 +2,38 @@
 /**
  * Demo app with SAML auth
  */
+
+if (isset($_GET['signin'])) {
+	require("/vagrant/SP/lib/_autoload.php");
+	$as = new SimpleSAML_Auth_Simple('default-sp');
+	$as->requireAuth();
+	if ($as->isAuthenticated()) {
+		$saml_attributes = $as->getAttributes();
+	}
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="">
+		<meta name="author" content="">
+
+		<title>Signin Template for Bootstrap</title>
+
+		<link href="styles/css/bootstrap.css" rel="stylesheet">
+		<link href="styles/css/signin.css" rel="stylesheet">
+	</head>
+
+	<body>
+		<div class="container">
+			<form class="form-signin" method="GET" action="?">
+				<input type="hidden" name="signin" value="" />
+				<button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+			</form>
+		</div>
+	</body>
+</html>
