a711576c7057d6a180393fdfecf5cf33c57ca7fd
Skylar Kelty <skylarkelty@gmail.com>
2013-10-26 14:18:53 +0100

    Add most SP config

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ Vagrant
 =======
 
 Vagrant Template for a SimpleSAMLPHP IdP and SP
+
+Add the following to your /etc/hosts file:
+```
+127.0.0.1 local.idp local.sp
+```
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 apt-get update
-apt-get install apache2 php5 -y
+apt-get install apache2 php5 php5-mcrypt memcached -y
 
 # Apache Config
 a2dissite default
-ln -s /vagrant/config/apache2.conf /etc/apache2/sites-enabled/saml
+ln -s /vagrant/config/apache2.conf /etc/apache2/sites-enabled/sp
+ln -s /vagrant/config/apache2-idp.conf /etc/apache2/sites-enabled/idp
 service apache2 reload
