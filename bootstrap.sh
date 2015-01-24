f015888d715250a7e05a179d7d153a42ecf4517a
Skylar Kelty <skylarkelty@gmail.com>
2015-01-24 16:59:37 +0000

    Start apache by default

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,7 @@ apt-get update
 apt-get install apache2 php5 php5-mcrypt php5-memcache memcached -y
 
 # Apache Config
+update-rc.d apache2 enable
 a2dissite default
 ln -s /vagrant/config/apache2.conf /etc/apache2/sites-enabled/default
 ln -s /vagrant/config/apache2-idp.conf /etc/apache2/sites-enabled/idp
